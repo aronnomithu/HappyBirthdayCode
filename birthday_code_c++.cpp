d1c28e23886edd9ceafe459b5a62b4364bbc5e0a
Aronno Mithu <31036977+aronnomithu@users.noreply.github.com>
2020-09-08 20:30:27 +0600

    Add files via upload

---
+++ birthday_code_c++.cpp
@@ -0,0 +1,60 @@
+#include<windows.h>
+#include<iostream>
+#include<stdlib.h> //for system("cls");
+using namespace std;
+
+int main()
+{
+ int i;
+ char h=3;
+ system("cls");
+ system("color f5"); //f->white background, 5->purple text
+ cout<<"\t\t\tFrom: Aronno Mithu\n";
+ Sleep(2500);
+ cout<<"\t\t\tTo  : Nova Rahman Shishir\n\n";
+ Sleep(2500);
+ cout<<"\t\t";
+ for(i=0;i<42;i++)
+ cout<<"_";
+ cout<<"\n\n";
+ system("color f9"); //f->white background, 9->light blue text
+
+ Sleep(2500);
+ cout<<"\t\t\t\ti i i i i\n";
+
+ Sleep(2500);
+ cout<<"\t\t\t\ti i i i i\n";
+
+ Sleep(2500);
+ cout<<"\t\t\t      __i_i_i_i_i__\n";
+
+ Sleep(2500);
+ cout<<"\t\t\t     |"<<h<<"           "<<h<<"|\n";
+
+ Sleep(2500);
+ cout<<"\t\t\t   __| "<<h<<"  25/08  "<<h<<" |__\n";
+
+ Sleep(2500);
+ cout<<"\t\t\t  | "<<h<<"   "<<h<<"       "<<h<<"   "<<h<<" |\n";
+
+ Sleep(2500);
+ cout<<"\t\t\t__|"<<h<<"   HAPPY    "<<h<<"   "<<h<<"|__\n";
+
+ Sleep(2500);
+ cout<<"\t\t       | "<<h<<"   "<<h<<"     BIRTHDAY    "<<h<<" |\n";
+
+Sleep(2500);
+ cout<<"\t\t       |"<<h<<"  "<<h<<"     "<<h<<"    "<<h<<"    "<<h<<"   "<<h<<" |\n";
+
+Sleep(2500);
+ cout<<"\t\t       | "<<h<<" "<<h<<" "<<h<<" "<<h<<" SHISHIR "<<h<<" "<<h<<" "<<h<<" "<<h<<" |\n";
+
+Sleep(2500);
+ cout<<"\t\t       |_________________________|\n";
+
+
+ cout<<"\n\n\n\n\n";
+ system("pause");
+ return 0;
+
+}
